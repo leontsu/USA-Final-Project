449a902b85a3e4e4f86c08fd86aaf4e8a5d7875d
Lazyturtle0852 <Kamenogakkou@gmail.com>
2025-07-03 17:00:46 +0900

    index.htmlに変更、そして対応・weather取得

--- js/api.js
+++ js/api.js
@@ -1,1 +1,26 @@
 //亀井の担当　open Weather APIなど
+function GetWeatherFromAPI() {
+    console.log("天気を検索中");
+    const apiKey = 'c470e678f1dab928aa51dbb7e8a9d6d2';
+
+    // 藤沢市の緯度と経度
+    const lat = 35.3377;
+    const lon = 139.4511;
+
+    // APIリクエストURL
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=ja`;
+
+    // 天気情報を取得して表示する
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                // サーバーからのエラー応答の場合、エラーを投げる
+                throw new Error('APIからの応答が正しくありません');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            return data.weather[0].main;
+        });
+}
--- js/processcontrol.js
+++ js/processcontrol.js
@@ -1,19 +1,45 @@
 //ここでは湘南台についたとき、SFCについたときそれぞれの処理がまとめて管理されます
 
-function ShonandaiFlow()
-{
+async function ShonandaiFlow() {
     console.log("main.htmlから、位置情報が湘南台だったので、ShonandaiFlowが呼び出されました");
 
-    //以下はイメージです。
-
     //api.jsを呼ぶ
+    //weatherTodayが今日の天気です。
+    try {
+        const weatherToday = await GetWeatherFromAPI();
+        console.log(weatherToday); 
+    } catch (error) {
+        console.error("天気の取得に失敗しました", error);
+    }
+
+    /*
+    note:weatherTodayは藤沢市の天気を、以下の要領で出力します
+    Clear 快晴
+    Clouds曇り
+    Rain雨
+    Drizzle霧雨
+    Snow雪
+    Thunderstorm雷雨
+    Atmosphere霧、もやなどの大気現象
+    */
+
+
     //database.jsを呼ぶ
+
+
     //gpt.jsを呼ぶ
 }
 
-function SFCFlow()
-{
+async function SFCFlow() {
     console.log("main.htmlから、位置情報がSFCだったので、SFCFlowが呼び出されました");
+    //api.jsを呼ぶ
+    try {
+        const weatherToday = await GetWeatherFromAPI();
+        console.log(weatherToday); 
+    } catch (error) {
+        console.error("天気の取得に失敗しました", error);
+    }
 
-    
+    //database.jsを呼ぶ
+    //gpt.jsを呼ぶ
 }
